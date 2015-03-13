1d7a7ed5afa951f1ddbd34a0cd9dbd22c1a35520
esmarr58 <ruben.estrada@hetpro.com.mx>
2015-03-13 11:57:41 -0600

    Update S4L2.cpp

--- S4/S4L2.cpp
+++ S4/S4L2.cpp
@@ -48,89 +48,14 @@ float var1 = 13.2;
 
 int main(void)
 {
-	//int temporal=0;
-	double A[3];
-	double B[3];
-	char nombre[30];
-	int i=0;
-	int f=0;
-	int toma=0;
-	int funciono = sprintf(	nombre, 
-				"imagen%d.png",
-				 numero);
-
-	string linea; 
-	string temporal;
-
-	if(!funciono) return -1;
-	
-	/*Parametros de conversion en imwrite*/
-	vector<int> compression_params;
-    	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    	compression_params.push_back(9);
-    	
-
-	
-	/*Inicia creacion del archivo*/
-	string  buffer;
-	ofstream info;
-	info.open("init.txt");
-
-	info << "Sanmina" << endl;
-	info << "Raspberry B+" << endl;
-	info << "Sesión 4" << endl;
-	info << X << " " << Y << endl;;
-	info << Verde 				<< " " << Rojo << " " << Azul << endl;
-	info << nombre << endl;
-	info.close();
-
-	/*Finaliza creacion de archivo*/
-	
-
-	/*Inicia lectura de archivo previamente creado*/
-	ifstream lectura("init.txt");
-	while(getline(lectura, linea)){
-	istringstream lectura_linea(linea);
-	if(i<3)
-		lectura_linea >> buffer;  //Desecha las primeras 3 lineas de texto
-	else if(i<5){
-		lectura_linea >> A[f] >> B[f]; //guarda en dos vectores las siguientes 2 lineas
-		f++;
-		if(f>2) f=0;
+	ofstream file("data.dat");
+	file << "#SanMIna" << endl;
+	file << "#x y" << endl;
+	for(int i=0; i<10; i++)
+	{
+		file << i << ' ' << i*i << endl;
 	}
-	else {
-		lectura_linea >> temporal;			//hace lectura del nombre del archivo a crear
-	}
-	
-	i++;
-
-	}
-
-	/*Finaliza lectura de archivo*/
-
-	/*Apertura de camara*/
-		
-	VideoCapture camara(0);
-	if(!camara.isOpened()) return -1;
-
-	Mat imagen;
-
-	/*Ciclo principal*/
-	
-	for(;;){
-		camara >> imagen;
-		toma++;
-	
-		if(toma == 5){
-		 try {
-        		imwrite(nombre, imagen, compression_params);
-	 }
-			break;
-		}
-
-
-	}
-	//Fin del programa
-
+	file.close();
+	system("gnuplot  plot.plt");
 	return 1;
 }
